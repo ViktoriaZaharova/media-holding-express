232155363adcaba2bfbc2ec6a2467e77c45ab7a1
Viktoria <zaharova181114@gmail.com>
2021-07-14 15:23:32 +0300

    add block

--- app/js/common.js
+++ app/js/common.js
@@ -5,3 +5,31 @@ $('.btn-burger').on('click', function () {
 $('.btn-close').on('click', function () {
     $('.mobile-menu').fadeOut();
 });
+
+$('.reviews-slider-content').slick({
+    slidesToShow: 1,
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    fade: true
+});
+
+$('.reviews-slider-video').slick({
+    slidesToShow: 1,
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    fade: true
+});
+
+$('a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
+    $('.reviews-slider-video').slick('setPosition');
+    $('.reviews-slider-content').slick('setPosition');
+});
+
+$('.advantages-slider').slick({
+    slidesToShow: 1,
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    fade: true,
+    dots: true,
+    infinite: false,
+});
